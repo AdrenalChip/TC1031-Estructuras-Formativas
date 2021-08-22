f017dfd21ea87d4a1de93329e326e835a2c827a4
Jorge Guerrero <jorge.guerrero.0023@gmail.com>
2021-08-22 11:53:08 -0500

    .H funciones de sumatorias

---
+++ Act 1.1 Funciones Iterativas, Recursivas y su análisis de Complejidad/funciones.h
@@ -0,0 +1,46 @@
+
+#ifndef funciones.h
+#define funciones.h
+
+#include <iostream>
+
+
+using namespace std;
+
+class Funciones{
+    public:
+    int sumaIterativa (int n);
+    int sumaRecursiva (int n);
+    int sumaDirecta (int n);
+};
+
+ int Funciones :: sumaIterativa(int n){
+    int acum=0;
+    while (n<0){
+        cout<<"N es un numero negativo,necesitas un numero postivo : ";
+        cin >> n;
+    }
+	for (int i=1;i<=n;i++){
+		acum+= i;
+	}
+	return acum;
+}
+
+int Funciones:: sumaRecursiva(int n){
+    while (n<0){
+        cout<<"N es un numero negativo,necesitas un numero postivo : ";
+        cin >> n;
+    }
+    if (n<=1){
+        return 1;
+    }else {
+        return n+(sumaRecursiva(n-1));
+    }
+}
+
+int Funciones :: sumaDirecta(int n){
+    int acum=0;
+    acum=n*(n+1)/2;
+    return acum;
+}
+#endif
